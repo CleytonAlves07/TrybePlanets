a4b8800d23a0451f150a5ebb6ebd252c0e1cf1e5
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-18 15:12:21 -0300

    Item 2 - Added the component SolarSystem

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>Incluir conteúdo!</h2>
+      </div>
+    );
+  }
+}
