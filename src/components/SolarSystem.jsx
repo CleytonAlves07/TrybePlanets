c16d5d95b404ea0cd2b0bdfc56f7c5adcf25899d
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-18 17:27:18 -0300

    Item 1, 2 and 3 - Modified for pass of tests

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 export default class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <h2>/Planetas/i</h2>
+        <Title headline="Planetas" />
       </div>
     );
   }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 export default class Title extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
       <div>
-        <h2> {this.props.headline}</h2>
+        <h2>
+          { headline }
+        </h2>
       </div>
     );
   }
 }
+
+// Title.propTypes = {
+  
+// }
