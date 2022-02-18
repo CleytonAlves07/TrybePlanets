2630f793996e0c6a90d56db054ef10dd4785e73a
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-18 20:23:26 -0300

    Item 6 - Added the name of planets and images

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planets.map(({ name, image }) => (<PlanetCard
+          key={ name }
+          planetImage={ image }
+          planetName={ name }
+        />))}
       </div>
     );
   }
